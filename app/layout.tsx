7237409027d92481c8571af2de5dbf0656f61175
Touhidur Zaman <touhidcodes@gmail.com>
2026-03-18 14:30:59 +0600

    fix: deploy test

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           <footer className="py-6 border-t md:px-8 md:py-0">
             <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
               <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-                Built by Touhidur Zaman. The source code is available on GitHub.
+                Built by Touhidur Zaman
               </p>
             </div>
           </footer>
