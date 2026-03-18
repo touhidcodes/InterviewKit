cc7860ca74ae4ca38a0db68ba2a21638502e1a57
Touhidur Zaman <touhidcodes@gmail.com>
2026-03-18 14:19:49 +0600

    fix: ci/cd

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Change 'interviewkit' to match your GitHub repo name
-  basePath: isProd ? '/interviewkit' : '',
-  assetPrefix: isProd ? '/interviewkit' : '',
+  // Match the casing of your GitHub repo name 'InterviewKit'
+  basePath: isProd ? '/InterviewKit' : '',
+  assetPrefix: isProd ? '/InterviewKit' : '',
   images: {
     unoptimized: true,
   },
